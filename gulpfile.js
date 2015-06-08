563b5ec7aaf25639d6cf1af1866dff89c1541520
Amitaibu <amitai@gizra.com>
2015-06-08 17:04:19 +0300

    Fix copy of shoovify

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task("fonts", function () {
 
 // Copy xml and txt files to the "site" directory
 gulp.task("copy", function () {
-  return gulp.src(["serve/*.txt", "serve/*.xml", "shoovify.sh"])
+  return gulp.src(["serve/*.txt", "serve/*.xml", "serve/shoovify.sh"])
     .pipe(gulp.dest("site"))
     .pipe($.size({ title: "xml & txt & shoovify.sh" }))
 });
