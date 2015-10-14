2cd9ab5ade2803f463b014bddd32d93b0d79616d
YaronMiro <yaron@gizra.com>
2015-10-14 11:50:49 +0300

    add javascript to decet querystring after from submittion

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -35,4 +35,21 @@ $(document).ready(function() {
       interval: 8000 
     });
 
+    // Display success message after form submit when redirecting to homepage.
+    // Checking if we have a query string with "?sent=true". if so, then display
+    // the success message.
+    var success = location.search.split('sent=')[1];
+    if (success) {
+       $('.alert-success').show();
+
+        // In case the browser supports then alter the url without refresh.
+        // Changing the url to homepage without any query string.
+        // This is done so the user will be able to refresh the page without the
+        // success message displaying without a redirect from the form submission.
+        if (history.pushState) {
+            var newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+            window.history.pushState({path: newUrl},'', newUrl);
+        }
+    }
+
 });
