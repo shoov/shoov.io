a88cd868bb336b6324edeef19e563961d8f57372
YaronMiro <yaron@gizra.com>
2015-10-14 19:03:49 +0300

    clean code

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -14,50 +14,12 @@ $(document).ready(function() {
     /* Ref: https://github.com/mathiasbynens/jquery-placeholder */
     $('input, textarea').placeholder();
 
-
-  /**
-   * Contact us form for setting "Shoov" trial submit callback.
-   */
-    var submitTrailForm = function(){
-
-      // The form element.
-      var $form = $('#trial-form');
-
-      // The form submit element.
-      var $submitButton = $form.find('button[type="submit"]');
-
-      $submitButton.bind('click', function(){
-        event.preventDefault();
-
-        // Get input values.
-        var website = $('input[type="url"]').val().trim();
-        var email = $('input[type="email"]').val().trim();
-
-        // Simple validation.
-        if (website == '' || email == '') {
-          $('.alert-danger').show();
-          return;
-        };
-
-        $.ajax({
-          url: $form.attr('action'),
-          method: "POST",
-          data: {
-            subject: 'shoov - contact trail set up',
-            url: website,
-            website:  email
-          },
-          dataType: "json",
-          success: function () {
-            $('.alert-danger').hide();
-            $('.alert-success').show();
-          }
-        });
-      })
-
-    }
-
-  // Form contact for trial submission callback.
-  submitTrailForm();
+  // Display success message after form submit when redirecting to homepage.
+  // Checking if we have a query string with "?sent=true". if so, then display
+  // the success message.
+  var success = location.search.split('sent=')[1];
+  if (success) {
+    $('.alert-success').show();
+  }
 
 });
