95e0733568458b1c9345dc7de78dead60f63b82e
YaronMiro <yaron@gizra.com>
2015-10-18 14:34:33 +0300

    Remove "fixed" header position and behavior

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
 
-    /* ======= Fixed header when scrolled ======= */    
-    $(window).on('scroll load', function() {
-         if ($(window).scrollTop() > 0) {
-          $('#header').addClass('scrolled');
-         }
-         else {
-          $('#header').removeClass('scrolled');
-         }
-    });
-
     /* ======= jQuery Placeholder ======= */
     /* Ref: https://github.com/mathiasbynens/jquery-placeholder */
     $('input, textarea').placeholder();
